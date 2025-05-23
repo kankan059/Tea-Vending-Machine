0faaec72a26a2a820745745ee7dbdd0b2db4df80
kankan ray <kankanray059@gmail.com>
2025-05-23 22:40:06 +0530

    save tea

--- database/save-tea.php
+++ database/save-tea.php
@@ -1,11 +1,12 @@
 <?php
 require '_dbconnect.php';
-
+session_start();
+$order_id = uniqid('order_');
 $data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('teas.txt' , print_r($data), true);
+file_put_contents('teas.txt', print_r($data), true);
 echo 'success fully recived';
 
-if($data){
+if ($data) {
     foreach ($data as $tea) {
         $tea_name = $tea['name'];
         $quantity = $tea['quantity'];
@@ -13,15 +14,12 @@ if($data){
         $price = $tea['price'];
         $cup = $tea['cup'];
         
-        $sql = "INSERT INTO `tea_orders` (`sno`, `tea_name`, `quantity`,`cup`,`sugar`, `price`, `time`) VALUES (NULL, '$tea_name', '$quantity', '$cup' ,'$sugar', '$price', current_timestamp())";
-
+        $sql = "INSERT INTO `tea_orders` (`sno`, `tea_name`, `quantity`,`cup`,`sugar`,`order_id`, `price`, `time`) VALUES (NULL, '$tea_name', '$quantity', '$cup' ,'$sugar','$order_id', '$price', current_timestamp())";
+        
         $connect->query($sql);
-
     }
     echo "data saved";
-    
-}
-else {
+    echo $order_id;
+} else {
     echo "not saved";
 }
-?>
